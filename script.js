919effce4f5e618074a8ef3835b437cc4d229c91
erlams <elmaddin@gmail.com>
2024-01-20 18:42:19 +0100

    wert

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-let a = alert('Bu oyunda siz latereya oynuyursuz: 1-10 regemleri yazirsiz kimde duzdu o gazandi');
-let b = prompt(' 1-10 regem yazin');
-
+let b = document.getElementById('num1');
 let netice;
 
-if (b == 1 || b == 3 || b == 8) {
-    netice = 'Sizi tebrik edirem.' + b + "regemi biz secdiyimiz regemdir. " + 'Siz bizden  1000$ gazandiz!';
-} else {
-    netice = 'Siz uduzduz 1000000$'  + b + "regemi biz istediyimiz regem deil. ";
+function topla() {
+    if (b.value == 1 || b.value == 3 || b.value == 8) {
+        netice = 'Sizi tebrik edirem. ' + b.value + " regemi biz secdiyimiz regemdir. " + 'Siz bizden  1000$ gazandiz!';
+    } else {
+        netice = 'Siz uduzduz 1000000$ '  + b.value + " regemi biz istediyimiz regem deil. ";
+    }
+    
+    document.getElementById('morro').innerHTML = netice;
 }
-
-
-
-
-document.getElementById('ayaz').innerHTML = netice;
-
